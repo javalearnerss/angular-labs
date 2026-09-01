d898ceba81ac2f58fa83689e9ef6246d5ec78a4c
Sandeep Yadav <33416624+javalearnerss@users.noreply.github.com>
2026-09-01 22:27:19 +0530

    Angular app and java project initial check-in

---
+++ book-gallery-server/src/main/java/com/bookgallery/server/BookGalleryServerApplication.java
@@ -0,0 +1,13 @@
+package com.bookgallery.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookGalleryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookGalleryServerApplication.class, args);
+	}
+
+}
---
+++ book-gallery-server/src/test/java/com/bookgallery/server/BookGalleryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookgallery.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookGalleryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
