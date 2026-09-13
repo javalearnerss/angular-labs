b38dd3873ff0601a68c14b9a3824648d3edcbf22
Sandeep Yadav <33416624+javalearnerss@users.noreply.github.com>
2026-09-14 01:49:16 +0530

    properties file changes

--- book-gallery/book-gallery-web/src/environments/environments.ts
+++ book-gallery/book-gallery-web/src/environments/environments.ts
@@ -1,6 +1,5 @@
-
 export const environment = {
-    production : true,
-    apiUrl : 'http://localhost:9030/api',
-    serverUrl: 'http://localhost:9030'
+    production: true,
+    apiUrl: '/api',
+    serverUrl: ''
 }
