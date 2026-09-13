528d569da565ffbe185406889ecacaa300d424c2
Sandeep Yadav <33416624+javalearnerss@users.noreply.github.com>
2026-09-14 01:48:33 +0530

    build files

--- book-gallery/book-gallery-server/src/main/java/com/bookgallery/server/controller/BooksController.java
+++ book-gallery/book-gallery-server/src/main/java/com/bookgallery/server/controller/BooksController.java
@@ -12,7 +12,6 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = "http://localhost:4200")
 public class BooksController {
 
     private static final Logger logger = LoggerFactory.getLogger(BooksController.class);
